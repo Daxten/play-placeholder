1c56ff2df6cb284501de62fc33db2ed975b24460
Alexej Haak <alexej.haak@outlook.de>
2014-10-05 19:57:20 +0200

    Using JavaImpl of File

--- app/Global.scala
+++ app/Global.scala
@@ -1,14 +1,17 @@
+import java.io.File
+
 import play.Logger
 import play.api.{Application, GlobalSettings}
 
-import scala.reflect.io.{Directory, File}
 
 /**
  * Created by Alexej on 05.10.2014.
  */
-object Global extends GlobalSettings{
+object Global extends GlobalSettings {
   override def onStart(app: Application) {
     Logger.info("placeholder-play starting")
-    Directory("public/placeholder").jfile.listFiles.map(_.delete)
+    val dir = new File("public/placeholder")
+    dir.mkdirs
+    dir.listFiles.map(_.delete)
   }
 }
